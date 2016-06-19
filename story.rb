b1121194ac19b6291229586a2305382ecb1f47dc
jnappy <jpnappy@gmail.com>
2016-06-19 13:33:36 -0400

    Update story.rb

--- story.rb
+++ story.rb
@@ -4,7 +4,9 @@ require 'pry'
   #the method should print the story title and the rating out of ten
 
 
-#show the user a message asking them for a title
+#show the user a message that says "Welcome to Movie Rating"
+
+#show the user a message asking them for the title of a movie
 
 #store the input in a variable
 
