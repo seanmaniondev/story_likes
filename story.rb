1201f0e656a0d9601aaa9739d9ce8b88e732d9ca
Sean Manion <seanmanion@Seans-MacBook-Pro.local>
2016-09-25 13:38:48 -0400

    a poorly written story

--- story.rb
+++ story.rb
@@ -3,10 +3,23 @@ require 'pry'
 #write a method that accepts two parameters: title and rating
   #the method should print the story title and the rating out of ten
 
+def display_story (title, rating)
+	puts("You've given the movie  '#{title}' a rating of #{rating}/10.")
+end
+
 
 #show the user a message that says "Welcome to Movie Rating"
 
+puts "Welcome to Movie Rating!"
+
+#binding.pry
 #show the user a message asking them for the title of a movie
+
+puts "Please enter a movie title"
+user_title = gets.strip 
+
+puts "Please enter a numerical rating 0-10 for this movie"
+user_rating = gets.strip
 
 #store the input in a variable
 
@@ -14,4 +27,4 @@ require 'pry'
 
 # store the input in a variable
 
-display_story(user_title, user_rating)
+results = display_story(user_title, user_rating)
